64422ea5dd68c294b3a2a051e5eece6b2e0a6637
=Dandi29022000 <=>
2021-05-29 11:19:43 +0700

    Membuat controller untuk produk

--- app/Http/Controllers/ProdukController.php
+++ app/Http/Controllers/ProdukController.php
@@ -4,46 +4,132 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 use App\Models\Product;
-use App\Models\Categories;
-use Illuminate\Support\Facades\DB;
+
 class ProdukController extends Controller
 {
-    //
-    public function index()
-    {
-        //Untuk menampilkan data produk yang dijoin dengan table kategori
-        //Saya kasih paginasi 9 data per halaman nya
-        $kat = DB::table('categories')
-                ->join('products','products.categories_id','=','categories.id')
-                ->select(DB::raw('count(products.categories_id) as jumlah, categories.*'))
-                ->groupBy('categories.id')
-                ->get();
-        $data = array(
-            'produks' => Product::paginate(9),
-            'categories' => $kat
-        );
-        return view('user.produk',$data);
-    }
-    public function detail($id)
-    {
-        //mengambil detail produk
-        $data = array(
-            'produk' => Product::findOrFail($id)
-        );
-        return view('user.produkdetail',$data);
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index(Request $request)
+    {	
+        $products = Product::where([
+            ['name','!=',Null],
+            [function($query)use($request){
+                if (($term = $request->term)) {
+                    $query->orWhere('name','LIKE','%'.$term.'%')
+                          ->orWhere('description','LIKE','%'.$term.'%')
+                          ->orWhere('weight','LIKE','%'.$term.'%')
+                          ->orWhere('price','LIKE','%'.$term.'%')->get();
+                }
+            }]
+        ])
+        ->orderBy('id','asc')
+        ->simplePaginate(5);
+        
+        return view('products.index' , compact('products'))
+        ->with('i',(request()->input('page',1)-1)*5);
     }
 
-    public function cari(Request $request)
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
     {
-        //mencari produk yang dicari user
-        $prod  = Product::where('name','like','%' . $request->cari. '%')->paginate(9);
-        $total = Product::where('name','like','%' . $request->cari. '%')->count(); 
-        $data  = array(
-            'produks' => $prod,
-            'cari' => $request->cari,
-            'total' => $total
-        );
-        return view('user.cariproduk',$data);
+        return view('products.create');
+    }
 
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        // Melakukan validasi data
+        $request->validate([
+            'id' => 'required',
+            'name' => 'required',
+            'description' => 'required',
+            'image' => 'required',
+            'weight' => 'required',
+        ]);
+
+        // Fungsi eloquent untuk menambah data
+        Product::create($request->all());
+
+        // Jika data berhasil ditambahkan, akan kembali ke halaman utama
+        return redirect()->route('products.index')
+            ->with('success', 'Produk Berhasil Ditambahkan');
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function show($id)
+    {
+        // Menampilkan detail data dengan menemukan/berdasarkan id_barang
+        $Product = Product::find($id);
+        return view('products.detail', compact('Product'));
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function edit($id)
+    {
+        // Menampilkan detail data dengan menemukan berdasarkan Nim Mahasiswa untuk diedit
+        $Product = Product::find($id);
+        return view('products.edit', compact('Product'));
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, $id)
+    {
+        // Melakukan validasi data
+        $request->validate([
+            'id' => 'required',
+            'name' => 'required',
+            'description' => 'required',
+            'image' => 'required',
+            'weight' => 'required',
+        ]);
+
+        // Fungsi eloquent untuk mengupdate data inputan kita
+        Product::find($id)->update($request->all());
+
+        // Jika data berhasil diupdate, akan kembali ke halaman utama
+        return redirect()->route('products.index')
+            ->with('success', 'Produk Berhasil Diupdate');
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy($id)
+    {
+        // Fungsi eloquent untuk menghapus data
+        Product::find($id)->delete();
+        return redirect()->route('products.index')
+            -> with('success', 'Produk Berhasil Dihapus');
     }
 }
